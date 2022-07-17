8d0c9341c6a869bef34beee053c2d52388761c01
Anirudh <hsanirudh@gmail.com>
2022-07-17 21:09:45 +0530

    Add some Options
    Signed-off-by: Anirudh <hsanirudh@gmail.com>

--- lua/src/basic.lua
+++ lua/src/basic.lua
@@ -1,17 +1,37 @@
--- local variables
-
-local set = vim.opt
-
 -- basic options 
 
-set.number = true
-set.autoindent = true
-set.tabstop = 2
-set.smarttab = true
-set.mouse = 'a'
-set.clipboard = 'unnamedplus'
-set.title = true
-set.termguicolors = true
+vim.o.pumheight = 10
+vim.o.fileencoding = 'utf-8'
+vim.o.cmdheight = 2
+vim.o.splitbelow = true
+vim.o.splitright = true
+vim.opt.termguicolors = true
+vim.o.conceallevel = 0
+vim.o.showtabline = 2
+vim.o.showmode = false
+vim.o.backup = false
+vim.o.writebackup = false
+vim.o.updatetime = 300
+vim.o.timeoutlen = 100
+vim.o.clipboard = "unnamedplus"
+vim.o.hlsearch = false
+vim.o.ignorecase = true
+vim.o.scrolloff = 3
+vim.o.sidescrolloff = 5
+vim.o.mouse = "a"
+vim.wo.wrap = false
+vim.wo.number = true
+vim.o.cursorline = true
+vim.wo.signcolumn = "yes"
+vim.o.tabstop = 2
+vim.bo.tabstop = 2
+vim.o.softtabstop = 2
+vim.o.shiftwidth = 2
+vim.bo.shiftwidth = 2
+vim.o.autoindent = true
+vim.bo.autoindent = true
+vim.o.expandtab = true
+vim.bo.expandtab = true
 
 -- init some plugins
 require('lualine').setup {
