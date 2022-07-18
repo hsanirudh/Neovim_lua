2c9befde2cc77da751a2310dfe3dd48a78bccdf7
Anirudh <hsanirudh@gmail.com>
2022-07-18 23:22:37 +0530

    Refactor Code
    Signed-off-by: Anirudh <hsanirudh@gmail.com>

--- lua/src/lspsaga.lua
+++ lua/src/lspsaga.lua
@@ -3,11 +3,11 @@ if not status_ok then
 	return
 end
 
-local saga = require 'lspsaga'
+local saga = require("lspsaga")
 
 saga.init_lsp_saga({
-				border_style = "single",
-				show_diagnostic_source = true,
-				diagnostic_source_bracket = {},
-				server_filetype_map = {}
+	border_style = "single",
+	show_diagnostic_source = true,
+	diagnostic_source_bracket = {},
+	server_filetype_map = {},
 })
