3faad7a51d769b7109303976251da13f47c9583f
hsanirudh <hsanirudh@gmail.com>
2022-10-18 01:31:26 +0530

    Update accordingly to documentation
    Previously using <update_capabalities> and changed to <default_capabalities>
    
    Signed-off-by: hsanirudh <hsanirudh@gmail.com>

--- lua/src/lsp/lang_server.lua
+++ lua/src/lsp/lang_server.lua
@@ -4,7 +4,7 @@ if not status_lsp_installer then
 end
 
 lsp_installer.on_server_ready(function(server)
-  local capabilities = require("cmp_nvim_lsp").update_capabilities(vim.lsp.protocol.make_client_capabilities())
+  local capabilities = require("cmp_nvim_lsp").default_capabilities(vim.lsp.protocol.make_client_capabilities())
   local opts = { capabilities = capabilities }
   if server.name == "sumneko_lua" then
     opts = vim.tbl_deep_extend("force", {
