405964613448f9257445cf263214185d4f664781
hsanirudh <hsanirudh@gmail.com>
2023-10-10 00:45:18 +0530

    plugins.lua -> Change entirely to lazy.nvim
    -> Changed the plugin manager to lazy.nvim from packer.nvim
    
    Signed-off-by: hsanirudh <hsanirudh@gmail.com>

--- lua/src/plugins.lua
+++ lua/src/plugins.lua
@@ -1,128 +1,114 @@
--- Automatically install packer If not
-local fn = vim.fn
-local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
-if fn.empty(fn.glob(install_path)) > 0 then
-	PACKER_BOOTSTRAP = fn.system({
-		"git",
-		"clone",
-		"--depth",
-		"1",
-		"https://github.com/wbthomason/packer.nvim",
-		install_path,
-	})
-	print("Installing packer close and reopen Neovim...")
-	vim.cmd([[packadd packer.nvim]])
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
 end
+vim.opt.rtp:prepend(lazypath)
 
--- Using pcall for debugging purpose
-local status_ok, packer = pcall(require, "packer")
-if not status_ok then
-	return
-end
-
--- plugins
-return packer.startup(function(use)
+require("lazy").setup({
 	-- packer.nvim plugin
-	use("wbthomason/packer.nvim") -- Have packer manage itself
-	use("nvim-lua/popup.nvim") -- An implementation of the Popup API from vim in Neovim
-	use("nvim-lua/plenary.nvim") -- Useful lua functions used ny lots of plugins
+	("wbthomason/packer.nvim"), -- Have packer manage itself
+	("nvim-lua/popup.nvim"), -- An implementation of the Popup API from vim in Neovim
+	("nvim-lua/plenary.nvim"), -- Useful lua functions used ny lots of plugins
 	-- MarkDown Preview
-	use({
+	({
 		"iamcco/markdown-preview.nvim",
 		run = function()
 			vim.fn["mkdp#util#install"]()
 		end,
-	})
+	}),
 	-- colorscheme
-	use("folke/tokyonight.nvim")
+	("folke/tokyonight.nvim"),
 	-- nvim-cmp and plugins
-	use("hrsh7th/cmp-nvim-lsp")
-	use("hrsh7th/cmp-cmdline")
-	use("hrsh7th/nvim-cmp")
-	use("hrsh7th/cmp-nvim-lua")
-	use("saadparwaiz1/cmp_luasnip")
-	use("L3MON4D3/LuaSnip")
-	use("hrsh7th/cmp-buffer")
-	use("hrsh7th/cmp-path")
-	use("rafamadriz/friendly-snippets")
+	("hrsh7th/cmp-nvim-lsp"),
+	("hrsh7th/cmp-cmdline"),
+	("hrsh7th/nvim-cmp"),
+	("hrsh7th/cmp-nvim-lua"),
+	("saadparwaiz1/cmp_luasnip"),
+	("L3MON4D3/LuaSnip"),
+	("hrsh7th/cmp-buffer"),
+	("hrsh7th/cmp-path"),
+	("rafamadriz/friendly-snippets"),
 	-- LSP and mason
-	use("jose-elias-alvarez/null-ls.nvim")
-	use({
+	("jose-elias-alvarez/null-ls.nvim"),
+	({
 		"williamboman/mason.nvim",
 		"williamboman/mason-lspconfig.nvim",
 		"neovim/nvim-lspconfig",
-	})
+	}),
 	-- Treesitter
-	use({ "nvim-treesitter/nvim-treesitter", run = ":TSUpdate" })
+	({ "nvim-treesitter/nvim-treesitter", run = ":TSUpdate" }),
 	-- Telescope
-	use("nvim-telescope/telescope.nvim")
+	("nvim-telescope/telescope.nvim"),
 	-- nvim-tree
-	use("kyazdani42/nvim-tree.lua")
-	use("kyazdani42/nvim-web-devicons")
+	("kyazdani42/nvim-tree.lua"),
+	("kyazdani42/nvim-web-devicons"),
 	-- dashboard.nivm
-	use({
+	({
 		"glepnir/dashboard-nvim",
-	})
+	}),
 	-- bufferline.lua
-	use({ "akinsho/bufferline.nvim", tag = "v3.*", requires = "nvim-tree/nvim-web-devicons" })
+	({ "akinsho/bufferline.nvim", tag = "v3.*", requires = "nvim-tree/nvim-web-devicons" }),
 	-- Lualine.nvim
-	use("nvim-lualine/lualine.nvim")
+	("nvim-lualine/lualine.nvim"),
 	-- gitsigns
-	use("lewis6991/gitsigns.nvim")
+	("lewis6991/gitsigns.nvim"),
 	-- comment
-	use("numToStr/Comment.nvim")
-	use("JoosepAlviste/nvim-ts-context-commentstring")
+	("numToStr/Comment.nvim"),
+	("JoosepAlviste/nvim-ts-context-commentstring"),
 	-- autopairs
-	use({
+	({
 		"windwp/nvim-autopairs",
 		config = function()
 			require("nvim-autopairs").setup({})
 		end,
-	})
+	}),
 	-- ns-auto-tag
-	use("windwp/nvim-ts-autotag")
+	("windwp/nvim-ts-autotag"),
 	-- diffview
-	use("sindrets/diffview.nvim")
-	-- lspsaga
-	use("glepnir/lspsaga.nvim")
+	("sindrets/diffview.nvim"),
+	-- lspsaga,
+	("glepnir/lspsaga.nvim"),
 	-- telescope-file-browser.nvim
-	use({ "nvim-telescope/telescope-file-browser.nvim" })
+	({ "nvim-telescope/telescope-file-browser.nvim" }),
 	-- colorizer
-	use({ "norcalli/nvim-colorizer.lua" })
+	({ "norcalli/nvim-colorizer.lua" }),
 	-- telescope-emoji
-	use({ "xiyaowong/telescope-emoji.nvim" })
+	({ "xiyaowong/telescope-emoji.nvim" }),
 	-- telescope-packer
-	use({ "nvim-telescope/telescope-packer.nvim" })
+	({ "nvim-telescope/telescope-packer.nvim" }),
 	-- which-key
-	use({ "folke/which-key.nvim" })
+	({ "folke/which-key.nvim" }),
 	-- wilder-nvim
-	use({ "gelguy/wilder.nvim" })
+	({ "gelguy/wilder.nvim" }),
 	-- indent-blackline
-	use({ "lukas-reineke/indent-blankline.nvim" })
+	({ "lukas-reineke/indent-blankline.nvim" }),
 	-- nvim-cursorline
-	use({ "yamatsum/nvim-cursorline" })
+	({ "yamatsum/nvim-cursorline" }),
 	-- better-escape.nvim
-	use({ "max397574/better-escape.nvim" })
+	({ "max397574/better-escape.nvim" }),
 	-- rainbow brackets
-	use({ "HiPhish/nvim-ts-rainbow2" })
+	({ "HiPhish/nvim-ts-rainbow2" }),
         -- Vim-eunuch
-        use({ "tpope/vim-eunuch" })
+        ({ "tpope/vim-eunuch" }),
         -- vim-fugative
-        use({ "tpope/vim-fugitive" })
+        ({ "tpope/vim-fugitive" }),
         -- vim-smoothie
-        use({ "psliwka/vim-smoothie" })
+        ({ "psliwka/vim-smoothie" }),
         -- code-minimap
-        use({ "wfxr/minimap.vim" })
+        ({ "wfxr/minimap.vim" }),
         -- error-lens
-        use {("chikko80/error-lens.nvim")}
+        {("chikko80/error-lens.nvim")},
         -- structlog.nvim
-        use {("Tastyep/structlog.nvim")}
+        {("Tastyep/structlog.nvim")},
         -- carbon.sh
-        use {( "ellisonleao/carbon-now.nvim" )}
+        {( "ellisonleao/carbon-now.nvim" )},
         -- numb
-        use {( "nacro90/numb.nvim" )}
-	-- Sync_Packer
-	if PACKER_BOOTSTRAP then
-		require("packer").sync()
-	end
-end)
+        {( "nacro90/numb.nvim" )},
+      })
