361dca81855aa5d59a8fa7b5aea1890fd5e1d554
hsanirudh <hsanirudh@gmail.com>
2023-10-10 00:16:12 +0530

    Indent-blackline.nvim->exclude dashboard and ibl
    -> After migrating to v3, exclude dashboard.nvim from indenting as it doesn't look good
    -> renamed the indentblackline to ibl which makes more sense
    
    Signed-off-by: hsanirudh <hsanirudh@gmail.com>

--- lua/src/ibl.lua
+++ lua/src/ibl.lua
@@ -21,6 +21,17 @@ hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
 end)
 
 vim.g.rainbow_delimiters = { highlight = highlight }
-require("ibl").setup { scope = { highlight = highlight } }
+require("ibl").setup { 
+  scope = { 
+    highlight = highlight 
+  },
+  exclude = {
+    filetypes = {
+      "dashboard"
+    },
+  },
+}
 
 hooks.register(hooks.type.SCOPE_HIGHLIGHT, hooks.builtin.scope_highlight_from_extmark)
+
+
